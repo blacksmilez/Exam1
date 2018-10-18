6d21315ee69cf62d4ce4664c4e5f833019e77655
blacksmilez <43358528+blacksmilez@users.noreply.github.com>
2018-10-18 07:59:48 -0400

    Update conn_inc.php
    database name was modified from 'cis485test1test1' to 'cis485test1'

--- php/conn_inc.php
+++ php/conn_inc.php
@@ -6,5 +6,5 @@
 $conn = mysqli_connect("127.0.0.1:3306", "root", "") or die(mysql_error());
 $db = mysqli_select_db($conn, "registration") or die(mysql_error($conn));
 $con = mysqli_connect("127.0.0.1:3306", "root", "") or die(mysql_error());
-$db2 = mysqli_select_db($con, "cis485test1test1") or die(mysql_error($con));
+$db2 = mysqli_select_db($con, "cis485test1") or die(mysql_error($con));
 ?>
